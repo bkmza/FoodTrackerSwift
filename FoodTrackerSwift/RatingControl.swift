b23c98f6419513d0855f46fbd5204ba9f0d3dbb1
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2018-07-02 16:02:04 +0300

    extended RatingControl

--- FoodTrackerSwift/RatingControl.swift
+++ FoodTrackerSwift/RatingControl.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
 
-    //MARK: Initialization
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
     
+    //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -23,27 +26,31 @@ class RatingControl: UIStackView {
     }
     
     //MARK: Button Action
-    
     @objc func ratingButtonTapped(button: UIButton) {
         print("Button pressed 👍")
     }
     
     //MARK: Private Methods
-    
     private func setupButtons() {
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
         
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 }
